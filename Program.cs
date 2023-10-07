b60d3bda862d2e57803f969fb8c9eed0ce179c8e
Enock Kimutai <92427438+enock-kim@users.noreply.github.com>
2023-10-07 23:02:42 +0300

    adde serilog and other changes

--- Program.cs
+++ Program.cs
@@ -5,20 +5,59 @@ using Microsoft.EntityFrameworkCore;
 using Prema.PriceHarborScraper.Workers;
 using Prema.PriceHarborScraper.Repository;
 using Prema.PriceHarborScraper.Policies;
+using Serilog;
+using Serilog.Events;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((hostContext, services) =>
+public class Program
+{
+    public static async Task<int> Main(string[] args)
     {
-        // Add MySQL DbContext
-        var connectionString = hostContext.Configuration.GetConnectionString("MySql");
-        services.AddDbContextPool<PriceHarborContext>(options => options.UseMySQL(connectionString));
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();    
 
-        //services.AddTransient<IRepository, Repository>();
-        services.AddSingleton<PollyPolicy>();
-        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-        services.AddHostedService<JumiaScraper>();
+        try
+        {
+            Log.Information("Starting host");
+            var host = BuildHost(args);
+            await host.RunAsync();
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Host terminated unexpectedly");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
 
-    })
-    .Build();
+    public static IHost BuildHost(string[] args) =>
+        new HostBuilder()
+            .ConfigureAppConfiguration((hostContext, configBuilder) =>
+            {
+                configBuilder
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                // Add MySQL DbContext
+                var connectionString = hostContext.Configuration.GetConnectionString("MySql");
+                services.AddDbContextPool<PriceHarborContext>(options => options.UseMySQL(connectionString));
 
-await host.RunAsync();
+                //services.AddTransient<IRepository, Repository>();
+                services.AddSingleton<PollyPolicy>();
+                services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+                services.AddHostedService<JumiaScraper>();
+
+            })
+            .UseSerilog() // <- Add this line
+            .Build();
+
+}
--- Workers/JumiaScraper.cs
+++ Workers/JumiaScraper.cs
@@ -5,6 +5,7 @@ using Prema.PriceHarborScraper.Models;
 using Prema.PriceHarborScraper.Policies;
 using Prema.PriceHarborScraper.Repository;
 using PuppeteerSharp;
+using Serilog;
 
 namespace Prema.PriceHarborScraper.Workers
 {
@@ -34,210 +35,140 @@ namespace Prema.PriceHarborScraper.Workers
             }
         }
 
-        //void Srcaper()
-        //{
-        //    // Send get request to jumia
-        //    //string url = "https://www.jumia.co.ke/samsung-galaxy-a04s-6.5-128gb-4gb-ram-dual-sim-50mp-5000mah-black-123460625.html";
-        //    string baseUrl = "https://www.jumia.co.ke";
-        //    string categoryUrl = "/smartphones/";
-        //    string url = baseUrl + categoryUrl;
-        //    var httpClient = new HttpClient();
-        //    List<Product> products = new List<Product>();
-
-        //    //// Get temp
-        //    //var itemElement = htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='-fs20 -pts -pbxs']");
-        //    //var item = itemElement.InnerText.Trim();
-        //    //Console.WriteLine($"Item: {item}");
-
-        //    //            // Get temp
-        //    //var priceElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='-b -ltr -tal -fs24 -prxs']");
-        //    //var price = priceElement.InnerText.Trim();
-        //    //Console.WriteLine($"Price: {price}");
-        //    bool validPage = true;
-        //    int pageCount = 1;
-        //    int deviceCount = 1;
-
-        //    while (validPage)
-        //    {
-        //        var html = httpClient.GetStringAsync($"{url}?page={pageCount}#catalog-listing").Result;
-        //        var htmlDocument = new HtmlDocument();
-        //        htmlDocument.LoadHtml(html);
-
-        //        //check if page has a result hence is valid
-        //        var divNode = htmlDocument.DocumentNode.SelectSingleNode($"//h2[@class='-pvs -fs16 -m']");
-
-        //        if (divNode != null)
-        //        {
-        //            validPage = divNode.InnerText.Trim().Equals("No results found!") ? false : true;
-
-        //            if (!validPage) continue;
-        //        }
-
-        //        // Specify the class name of the div you want to target
-        //        string targetDivClass = "-paxs row _no-g _4cl-3cm-shs";
-
-        //        // Find the div element with the specified class
-        //        divNode = htmlDocument.DocumentNode.SelectSingleNode($"//div[@class='{targetDivClass}']");
-
-        //        pageCount++;
-
-        //        if (divNode != null)
-        //        {
-        //            // Find all <a> elements within the div
-        //            var hrefAttributes = divNode.Descendants("a")
-        //                .Select(a => a.GetAttributeValue("href", string.Empty))
-        //                .ToList();
-
-        //            // Print the href attributes
-        //            foreach (var href in hrefAttributes)
-        //            {
-        //                var product = new Product()
-        //                {
-        //                    Name = href,
-        //                    PlatformId = 1,
-        //                    ManufacturerId = 1,
-        //                    ProductGroupId = 1,
-        //                    Link = baseUrl + href
-        //                };
-        //                products.Add(product);
-        //                Console.WriteLine(href);
-        //                deviceCount++;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("Div with class not found.");
-        //        }
-        //        Thread.Sleep(600);
-                   
-        //    }
-
-        //    GetManufacturer(products);
-
-        //    using (var scope = _serviceProvider.CreateScope())  
-        //    {
-        //            var scopedProductRepository = scope.ServiceProvider.GetRequiredService<IRepository<Product>>();
-        //        scopedProductRepository.AddList(products);
-        //    }
-        //    Console.WriteLine($"Total Items Found: {deviceCount}");
-        //}
-
         public async Task Scraper()
         {
             var proxyServers = new[]
                 {
-                    "86.96.15.70:8080",
-                    "202.154.36.57:8080",
-                    "179.42.9.242:3128",
-                    "196.251.134.147:8080",
-                    "117.71.149.66:8089",
-                    "207.2.120.19:80",
-                    "45.171.108.6:999",
-                    "64.225.8.142:10006",
-                    "133.242.229.79:33333",
-                    "34.82.224.175:33333",
-                    "188.166.17.18:8881",
-                    "124.167.20.48:7777"
+                    "154.79.254.236:32650",
+                    "197.248.86.237:32650",
+                    "197.232.47.122:8080",
+                    "197.254.84.86:32650",
+                    "102.0.3.222:8080",
+                    "154.79.248.44:32650",
+                    "197.254.11.250:32650",
+                    "196.216.65.57:8080",
+                    "80.240.202.218:8080",
+                    "197.232.36.85:41890",
+                    "102.0.2.104:8080",
+                    "197.254.99.58:8080",
+                    "154.79.251.210:32650",
+                    "197.232.65.40:55443"
                 };
 
             var proxyServerArgument = string.Join(",", proxyServers);
+            int proxyCount = 0;
+            bool complete = false;
 
-            var launchOptions = new LaunchOptions
+            while (!complete)
             {
-                Headless = true,
-                Args = new[] { $"--proxy-server={proxyServerArgument}" }
-            };
 
-            _logger.LogInformation("Fetching browser.");
-            await new BrowserFetcher().DownloadAsync();
-            _logger.LogInformation("Launching browser.");
-            using var browser = await Puppeteer.LaunchAsync(launchOptions);
-            _logger.LogInformation("Launching new page.");
-            using var page = await browser.NewPageAsync();
-
-            string baseUrl = "https://www.jumia.co.ke";
-            string categoryUrl = "/smartphones/";
-            string url = baseUrl + categoryUrl;
-            List<Product> products = new List<Product>();
-            bool validPage = true;
-            int pageCount = 1;
-            int deviceCount = 1;
-
-            while (validPage)
-            {
-                _logger.LogInformation("Opening new link.");
-                //await page.GoToAsync($"{url}?page={pageCount}#catalog-listing");
-                await _polly.LinearHttpRetry.ExecuteAsync(async () => 
+                var launchOptions = new LaunchOptions
                 {
-                    try 
+                    Headless = true,
+                    Args = new[] { $"--proxy-server={proxyServers[proxyCount]}" }
+                };
+
+                _logger.LogInformation("Fetching browser.");
+                await new BrowserFetcher().DownloadAsync();
+                _logger.LogInformation("Launching browser.");
+                using var browser = await Puppeteer.LaunchAsync(launchOptions);
+                _logger.LogInformation("Launching new page.");
+                using var page = await browser.NewPageAsync();
+
+                string baseUrl = "https://www.jumia.co.ke";
+                string categoryUrl = "/smartphones/";
+                string url = baseUrl + categoryUrl;
+                List<Product> products = new List<Product>();
+                bool validPage = true;
+                int pageCount = 1;
+                int deviceCount = 1;
+
+                while (validPage && !complete)
+                {
+                    _logger.LogInformation($"Opening new link. Page number: {pageCount}");
+                    //await page.GoToAsync($"{url}?page={pageCount}#catalog-listing");
+                    try
                     {
-                        await page.GoToAsync($"{url}?page={pageCount}#catalog-listing");
+                        await _polly.LinearHttpRetry.ExecuteAsync(async () =>
+                        {
+                            try
+                            {
+                                await page.GoToAsync($"{url}?page={pageCount}#catalog-listing");                    
+                                // Wait for the page to load, you can adjust the delay as needed
+                                await page.WaitForTimeoutAsync(3000);
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.LogError($"GoToAsync: {ex.Message}");
+                                throw;
+                            }
+                        });
                     }
                     catch (Exception ex)
                     {
-                        _logger.LogError($"GoToAsync: {ex.Message}");
-                        throw;
+                        _logger.LogError($"Error: {ex.Message}");
+                        _logger.LogError($"Problem with proxy {proxyServers[proxyCount]}, trying new proxy {proxyServers[proxyCount == proxyServers.Length - 1 ? 0 : proxyCount + 1]}.");
+                        proxyCount = proxyCount == proxyServers.Length - 1 ? 0 : proxyCount + 1; 
+                        continue;
                     }
-                });
 
-                // Wait for the page to load, you can adjust the delay as needed
-                await page.WaitForTimeoutAsync(3000);
 
-                var divNode = await page.QuerySelectorAsync("h2.-pvs.-fs16.-m");
+                    var divNode = await page.QuerySelectorAsync("h2.-pvs.-fs16.-m");
 
-                if (divNode != null)
-                {
-                    var divText = await divNode.EvaluateFunctionAsync<string>("el => el.innerText");
-                    validPage = divText.Trim().Equals("No results found!", StringComparison.OrdinalIgnoreCase) ? false : true;
-
-                    if (!validPage) continue;
-                }
-
-                var targetDivClass = "-paxs.row._no-g._4cl-3cm-shs";
-                var div = await page.QuerySelectorAsync($"div.{targetDivClass}");
-
-                pageCount++;
-
-                if (div != null)
-                {
-                    var links = await div.QuerySelectorAllAsync("a");
-
-                    _logger.LogInformation("Processing data.");
-                    foreach (var link in links)
+                    if (divNode != null)
                     {
-                        var href = await link.EvaluateFunctionAsync<string>("el => el.getAttribute('href')");
-                        var product = new Product
-                        {
-                            Name = href,
-                            PlatformId = 1,
-                            ManufacturerId = 1,
-                            ProductGroupId = 1,
-                            Link = baseUrl + href
-                        };
-                        products.Add(product);
-                        Console.WriteLine(href);
-                        deviceCount++;
+                        var divText = await divNode.EvaluateFunctionAsync<string>("el => el.innerText");
+                        validPage = divText.Trim().Equals("No results found!", StringComparison.OrdinalIgnoreCase) ? false : true;
+
+                        if (!validPage) continue;
                     }
+
+                    var targetDivClass = "-paxs.row._no-g._4cl-3cm-shs";
+                    var div = await page.QuerySelectorAsync($"div.{targetDivClass}");
+
+                    pageCount++;
+
+                    if (div != null)
+                    {
+                        var links = await div.QuerySelectorAllAsync("a");
+
+                        _logger.LogInformation("Processing data.");
+                        foreach (var link in links)
+                        {
+                            var href = await link.EvaluateFunctionAsync<string>("el => el.getAttribute('href')");
+                            var product = new Product
+                            {
+                                Name = href,
+                                PlatformId = 1,
+                                ManufacturerId = 1,
+                                ProductGroupId = 1,
+                                Link = baseUrl + href
+                            };
+                            products.Add(product);
+                            Console.WriteLine(href);
+                            deviceCount++;
+                        }
+                    }
+                    else
+                    {
+                        _logger.LogWarning("Div with class not found.");
+                    }
+
+                    // You can adjust the delay as needed
+                    await Task.Delay(600);
                 }
-                else
+
+                _logger.LogInformation($"Match manufacturers.");
+                GetManufacturer(products);
+
+                using (var scope = _serviceProvider.CreateScope())
                 {
-                    _logger.LogWarning("Div with class not found.");
+                    var scopedProductRepository = scope.ServiceProvider.GetRequiredService<IRepository<Product>>();
+                    scopedProductRepository.AddList(products);
                 }
-
-                // You can adjust the delay as needed
-                await Task.Delay(600);
+                _logger.LogInformation($"Total Items Found: {deviceCount}");
+                complete = true;
             }
-
-            _logger.LogInformation($"Match manufacturers.");
-            GetManufacturer(products);
-
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var scopedProductRepository = scope.ServiceProvider.GetRequiredService<IRepository<Product>>();
-                scopedProductRepository.AddList(products);
-            }
-
-            _logger.LogInformation($"Total Items Found: {deviceCount}");
         }
 
         public void GetManufacturer(List<Product> products)
